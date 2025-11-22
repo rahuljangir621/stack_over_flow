7e71bd43f94b5022aba305d5ca51ec917a9105f0
rahuljangir621 <rahuljangir621@gmail.com>
2025-11-22 17:36:03 +0530

    Update wrapper.java

--- src/main/java/demo/wrapper.java
+++ src/main/java/demo/wrapper.java
@@ -16,7 +16,6 @@ public class wrapper {
     ChromeDriver driver;
     WebDriverWait wait;
      
-
     public wrapper(ChromeDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
